d0eab1f481a8b2afcc376ea4338342e9e094942e
Theodor Skippervold <theodorskippervold@Theodors-MacBook-Pro-2.local>
2018-01-14 17:50:40 +0100

    Added iOS and tvOS targets

---
+++ Sources/MyTest.swift
@@ -0,0 +1,8 @@
+//
+//  MyTest.swift
+//  SonosLib iOS
+//
+//  Created by Theodor Skippervold on 14.01.2018.
+//
+
+import Foundation
---
+++ Sources/SonosLib.h
@@ -0,0 +1,18 @@
+//
+//  SonosLib_iOS.h
+//  SonosLib iOS
+//
+//  Created by Theodor Skippervold on 14.01.2018.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SonosLib_iOS.
+FOUNDATION_EXPORT double SonosLibVersionNumber;
+
+//! Project version string for SonosLib_iOS.
+FOUNDATION_EXPORT const unsigned char SonosLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SonosLib_iOS/PublicHeader.h>
+
+
